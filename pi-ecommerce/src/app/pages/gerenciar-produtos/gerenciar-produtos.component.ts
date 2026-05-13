5743a8b4584ac0cb16c84ba4568dff5c471a2ec5
LUCIANO LUIS DE SOUSA JUNIOR <luciano.lsjunior1@senacsp.edu.br>
2026-05-12 21:55:04 -0300

    RAiz feita

---
+++ pi-ecommerce/src/app/pages/gerenciar-produtos/gerenciar-produtos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gerenciar-produtos',
+  imports: [],
+  templateUrl: './gerenciar-produtos.component.html',
+  styleUrl: './gerenciar-produtos.component.scss'
+})
+export class GerenciarProdutosComponent {
+
+}
