16cc5ceeb47f0ae17ab9504efc2e7752b7e8f9df
Luiz Felipe Correia <lfc291006@gmail.com>
2026-05-20 12:15:03 -0300

    Adicionando projeto Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./shared/navbar/navbar";
+import { Home } from "./componentes/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
