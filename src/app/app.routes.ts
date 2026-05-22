99fccc589e792892880fa54803f34284ae8fc525
Luiz Felipe Correia <lfc291006@gmail.com>
2026-05-22 17:15:38 -0300

    Adiciona rotas para a página de produtos e atualiza links de navegação para usar routerLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 
 import { GerenciarProdutosComponent } from './componentes/gerenciar-produtos/gerenciar-produtos';
 import { Home } from './componentes/home/home';
+import { produtos } from './componentes/produtos/produtos';
 
 export const routes: Routes = [
 
-  { path: '', component: Home, title: 'Home' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home, title: 'TechStore - Home' },
+  { path: 'produtos' , component: produtos, title: 'TechStore - Produtos'},
   { path: 'gerenciar-produtos', component: GerenciarProdutosComponent, title: 'Gerenciar - Produtos' },
+  /* { path: 'login', component: Home, title: 'TechStore - Login' },
+  { path: 'carrinho', component: Home, title: 'TechStore - Carrinho' } */
+   //adicionar deopois que gerar os componentes
+
 
 ];
