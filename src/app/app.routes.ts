56b3c34bf756aacf820e62bd8b6e7b705b6c80e7
Luiz Felipe Correia <lfc291006@gmail.com>
2026-05-22 22:42:17 -0300

    Adição da página do carrinho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,19 @@ import { Routes } from '@angular/router';
 import { GerenciarProdutosComponent } from './componentes/gerenciar-produtos/gerenciar-produtos';
 import { Home } from './componentes/home/home';
 import { produtos } from './componentes/produtos/produtos';
+import { Carrinho } from './componentes/carrinho/carrinho';
 
 export const routes: Routes = [
 
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home, title: 'TechStore - Home' },
   { path: 'produtos' , component: produtos, title: 'TechStore - Produtos'},
-  { path: 'gerenciar-produtos', component: GerenciarProdutosComponent, title: 'Gerenciar - Produtos' },
+  { path: 'gerenciar-produtos', component: GerenciarProdutosComponent, title: 'TechStore - Gerenciar Produtos' },
+  { path: 'carrinho', component: Carrinho, title: 'TechStore - Carrinho' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
   /* { path: 'login', component: Home, title: 'TechStore - Login' },
-  { path: 'carrinho', component: Home, title: 'TechStore - Carrinho' } */
    //adicionar deopois que gerar os componentes
+   */
 
 
 ];
---
+++ src/app/componentes/carrinho/carrinho.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-carrinho',
+  imports: [RouterLink],
+  templateUrl: './carrinho.html',
+  styleUrls: [
+    './carrinho.css',
+    '../home/home.css'
+],
+})
+export class Carrinho {}
