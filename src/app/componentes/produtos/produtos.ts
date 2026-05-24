21b76fc8bf20272bf358c7be078dd3d063d5dc44
Luiz Felipe Correia <lfc291006@gmail.com>
2026-05-24 19:51:29 -0300

    Adição da rota para gerenciar produtos

--- src/app/componentes/produtos/produtos.ts
+++ src/app/componentes/produtos/produtos.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-produtos',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './produtos.html',
   styleUrl: './produtos.css',
 })
